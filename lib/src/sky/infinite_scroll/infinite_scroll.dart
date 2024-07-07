3cfa13179b903ce6be99481a16488ee749376c79
huangyuchao <87007@topscore.com.cn>
2024-07-07 23:36:17 +0800

    1
    1

--- lib/src/sky/infinite_scroll/infinite_scroll.dart
+++ lib/src/sky/infinite_scroll/infinite_scroll.dart
@@ -10,12 +10,14 @@ class InfiniteScroll<T> extends StatefulWidget {
     required this.itemBuilder,
     this.loadMore,
     this.controller,
+    this.scrollbars = true,
   });
   final bool loadFinish;
   final List<T> data;
   final Widget? Function(BuildContext, int) itemBuilder;
   final Function()? loadMore;
   final ScrollController? controller;
+  final bool? scrollbars;
   @override
   State<InfiniteScroll> createState() => _InfiniteScrollState();
 }
@@ -23,14 +25,16 @@ class InfiniteScroll<T> extends StatefulWidget {
 class _InfiniteScrollState extends State<InfiniteScroll> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        controller: widget.controller,
-        itemCount: widget.data.length,
-        itemBuilder: (context, index) {
-          if (index == widget.data.length - 1 && widget.loadMore != null && !widget.loadFinish) {
-            widget.loadMore!.call();
-          }
-          return widget.itemBuilder(context, index);
-        });
+    return ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: widget.scrollbars),
+        child: ListView.builder(
+            controller: widget.controller,
+            itemCount: widget.data.length,
+            itemBuilder: (context, index) {
+              if (index == widget.data.length - 1 && widget.loadMore != null && !widget.loadFinish) {
+                widget.loadMore!.call();
+              }
+              return widget.itemBuilder(context, index);
+            }));
   }
 }
