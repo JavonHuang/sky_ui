ab261be9389732c1c07d9ec2afd28c151677b8bb
huangyuchao <87007@topscore.com.cn>
2024-08-19 23:45:15 +0800

    2
    2

--- lib/src/sky/date_picker/model/sky_data_picker_utils.dart
+++ lib/src/sky/date_picker/model/sky_data_picker_utils.dart
@@ -1,6 +1,7 @@
 import '../../../utils/utils.dart';
 
 class SkyDataPickerUtils extends SkyMoment {
+  static List<String> headerTitle = ["日", "一", "二", "三", "四", "五", "六"];
   int getDaysInFebruary(int year) {
     return DateTime(year, 2, 29).difference(DateTime(year, 2, 28)).inDays;
   }
